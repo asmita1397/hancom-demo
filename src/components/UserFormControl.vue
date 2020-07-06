54b2ee31eb6c06267c5a703b9987e80fc03cd4ee
Asmita Shenvi <asmitashenvi97@gmail.com>
2020-07-06 14:19:38 +0530

    deleting the selected control on backspace

--- src/components/UserFormControl.vue
+++ src/components/UserFormControl.vue
@@ -14,11 +14,11 @@
           :parent="true"
           @resizing="(x,y,width,height)=>onResize(control,x,y,width,height)"
           @dragstop="(left, top) => dragstop(control, left, top)"
-        
+          @deactivated="onDeactivated"
+          @activated="onActivated(modal,control)"
         >
           <CustomLabel v-if="control.type==='Label'" :control="control" :modal="modal" />
           <CustomButton v-if="control.type==='CommandButton'" :control="control" :modal="modal" />
-
         </vue-draggable-resizable>
       </template>
     </div>
@@ -45,6 +45,8 @@ export default {
         position: "absolute",
         backgroundColor: ""
       },
+       deletingControlId: -1,
+      deletingUserFormId: -1,
       style: {
         zIndex: 20
       }
@@ -53,8 +55,39 @@ export default {
   props: {
     modal: Object
   },
+  mounted() {
+    document.addEventListener("keydown", this.handleKeyDown);
+  },
 
   methods: {
+    handleKeyDown(e) {
+      console.log(e);
+      if (e.keyCode === 8) {
+       
+        if (this.deletingUserFormId !== -1 && this.deletingControlId !== -1) {
+          console.log("==============", this.deletingUserFormId);
+          console.log("=============", this.deletingControlId);
+
+          if (this.modal.id === this.deletingUserFormId) {
+            this.modal.controls.splice(this.deletingControlId, 1);
+            this.deletingControlId = -1;
+            this.deletingUserFormId = -1;
+          }
+        }
+      }
+    },
+     onDeactivated() {
+      this.deletingControlId = -1;
+      this.deletingUserFormId = -1;
+    },
+    onActivated(modal, control) {
+      for (let i = 0; i < modal.controls.length; i++) {
+        if (modal.controls[i].id === control.id) {
+          this.deletingControlId = i;
+          this.deletingUserFormId = modal.id;
+        }
+      }
+    },
     onResize(control, x, y, width, height) {
       control.style.width = `${width}px`;
       control.style.height = `${height}px`;
@@ -64,8 +97,7 @@ export default {
     dragstop(control, x, y) {
       control.style.left = `${x}px`;
       control.style.top = `${y}px`;
-    },
-   
+    }
   }
 };
 </script>
